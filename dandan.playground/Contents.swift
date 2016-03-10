62c003c706d3f6c5dddc8fe4c9abfc7ac46125e2
yudandan323 <15771335642@163.com>
2016-03-10 11:08:41 +0800

    shuzu

---
+++ dandan.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+var hours=24
+
+/*func add(x:Int,y:Int){
+    print("\(x)+\(y)=\(x+y)")
+}
+add(10,20)*/
+var PI=3.14
+var swiftIsFun=true
+var boxue="boxue.io"
+var me = ("Mars",11,"11@boxue.io")
+me.0
+me.1
+me.2
+hours=12
+let minutes=60
+let fireIsHot=true
+var x:Int
+var s:String
+//print（“\x”）
+
+
+
+
+

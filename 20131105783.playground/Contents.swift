3ea8a16e6c70ba1c2a66e66cbadd5c0d0684514e
yudandan323 <15771335642@163.com>
2016-03-10 11:30:08 +0800

    在数组中添加元素、删除元素和遍历数组的方法

--- 20131105783.playground/Contents.swift
+++ 20131105783.playground/Contents.swift
@@ -6,3 +6,19 @@ var shuzu=[1,2,3,4,5]
 if shuzu.isEmpty{
     print("数组是空的")
 }
+//数组读取元素个数
+shuzu.count
+//修改数组
+shuzu[0...1]=[6,5]
+//插入元素
+shuzu.insert(7, atIndex: 0)
+//删除数组中的元素
+shuzu.removeAtIndex(0)
+//删除数组的最后一个元素
+shuzu.removeLast()
+//遍历数组
+for number  in shuzu{
+    print(number)
+}
+
+

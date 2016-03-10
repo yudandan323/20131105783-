b1d60ed25e8b1265c48509b3a6d606373db286ff
yudandan323 <15771335642@163.com>
2016-03-10 11:59:35 +0800

    无序数组

--- 20131105783.playground/Contents.swift
+++ 20131105783.playground/Contents.swift
@@ -1,7 +1,7 @@
 //: Playground - noun: a place where people can play
 
 import Cocoa
-
+//有序集合
 var shuzu=[1,2,3,4,5]
 if shuzu.isEmpty{
     print("数组是空的")
@@ -18,6 +18,33 @@ shuzu.removeAtIndex(0)
 shuzu.removeLast()
 //遍历数组
 for number  in shuzu{
+    print(number)
+}
+//无序集合
+let number=10
+number.hashValue
+let PI=3.14
+PI.hashValue
+//通过属性读取它的哈希值
+let string="dandan"
+string.hashValue
+//定义包含字符类型的空集合
+let emptySet:Set<Character>=Set<Character>()
+//emptySet=Set<Character>()
+let zifu:Set<Character>=["a","e","u","p","l"]
+var evenSet:Set=[2,4,6,9,0]
+//数组读取元素个数
+zifu.count
+//
+zifu.isEmpty
+//
+evenSet.insert(3)
+//
+evenSet.remove(3)
+//
+evenSet.contains(0)
+//
+for number in evenSet{
     print(number)
 }
 

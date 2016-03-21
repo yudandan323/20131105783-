bb3bd154ffe16b89caae94625225f276d1a03a82
yudandan323 <15771335642@163.com>
2016-03-21 21:08:12 +0800

    做的界面，与命名

---
+++ Calulator/Calulator/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Calulator
+//
+//  Created by 于丹丹 on 16/3/21.
+//  Copyright (c) 2016年 于丹丹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Clear: UIButton!
+    @IBOutlet weak var Pane: UIButton!
+    @IBOutlet weak var Percentage: UIButton!
+    @IBOutlet weak var Divide: UIButton!
+    @IBOutlet weak var seven: UIButton!
+    @IBOutlet weak var eight: UIButton!
+    @IBOutlet weak var nine: UIButton!
+    @IBOutlet weak var four: UIButton!
+    @IBOutlet weak var five: UIButton!
+    @IBOutlet weak var six: UIButton!
+    @IBOutlet weak var one: UIButton!
+    @IBOutlet weak var two: UIButton!
+    @IBOutlet weak var three: UIButton!
+    @IBOutlet weak var zero: UIButton!
+    @IBOutlet weak var piont: UIButton!
+    @IBOutlet weak var Multiplication: UIButton!
+    @IBOutlet weak var Sub: UIButton!
+    @IBOutlet weak var add: UIButton!
+    @IBOutlet weak var equal: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
